1842ea473d2492f2224be397909b0092c4fa90ee
Argie <argie.b@flowerstore.ph>
2024-02-27 18:32:02 +0800

    fix screenwidth run build

---
+++ src/components/Loader.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const Loader = () => {
+  return (
+    <div className='loader-main min-h-screen flex justify-center items-center'>
+      Loading....
+    </div>
+  )
+}
