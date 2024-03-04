454bbddcd2701c19378b09ce7d159928748389a3
Loanne <58616743+Loanne05@users.noreply.github.com>
2024-03-04 13:54:13 +0800

    Blank Header, Footer data from general settings

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,8 +5,7 @@ export const Header = () => {
   return (
     <div className="header-main w-full flex justify-center items-center bg-[#2E426C] text-white font-bold py-4">
       <div className="flex gap-2">
-        <Image src={logo} height={25} width={25} alt="book" priority />        
-        FS RIDER APP
+
       </div>
     </div>
   )
