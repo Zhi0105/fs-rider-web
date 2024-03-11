63f74e2edd32d1ad2394602e75303463485a5508
Argie <argie.b@flowerstore.ph>
2024-03-11 16:07:11 +0800

    updates

---
+++ src/providers/RTOProviders.tsx
@@ -0,0 +1,42 @@
+'use client';
+import React from 'react';
+import { RTOContext } from './context/RTOContext';
+import { useMutation, useQueryClient  } from "@tanstack/react-query";
+import { RTODataInterface } from '@_types/RTO/interface';
+import { SentRTODev } from '@_api/rto/rto';
+import { toast } from "react-toastify"
+
+export const RTOProviders = ({ children }: { children: React.ReactNode }) => {
+  const queryClient = useQueryClient();
+
+
+  const { mutate: handleSentRTODev, isLoading: RTODevLoading, error: RTOErr } = useMutation({
+    mutationFn: SentRTODev,
+    onSuccess: (data: any) => {
+        queryClient.invalidateQueries({ queryKey: ['dev-rto'] });
+        toast("Sent Successful", { type: "success" })
+        console.log("@RTOD:", data)
+      }, 
+    onError: (err: any) => {  
+      toast(err.response.data.message, { type: "warning" })
+      console.log("@RTODE:", err)
+    },
+  });
+
+  const handleSendRTO = ( data: RTODataInterface, resetData: () => void) => {
+    handleSentRTODev(data)
+    // !RTOErr && !RTODevLoading && resetData()
+  }
+
+  return (
+    <RTOContext.Provider
+      value={{
+        handleRTO(data, resetData) {
+          handleSendRTO(data, resetData)
+        },
+      }}
+    >
+      {children}
+    </RTOContext.Provider>
+  )
+}
